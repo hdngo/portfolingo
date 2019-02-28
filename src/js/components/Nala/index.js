e3dc52ac64f2c25c7533c7dcf9161124628bf470
Harvey Ngo <harveydngo@gmail.com>
2019-02-27 17:37:54 -0800

    Add starter files for Nala component. Todo: Fix da sleep sched and run errands. Tododo: Work on other pages and React Router.

---
+++ src/js/components/Nala/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+export default class Nala extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
+
+    render() {
+        return (
+            <div className='nalas-playpen'>
+                <div className='nala'>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                    <div className='nala-fur'></div>
+                </div>
+            </div>
+        )
+    }
+}
