d228b56b200714115078c950f8fc43c224cb7e2e
Harvey Ngo <harveydngo@gmail.com>
2019-03-05 20:20:15 -0800

    Tweak transitions for Splash and Home pages. Begin adding work screenshots. Todo: Gym, eat, walk dog, etc. Todotodo: Integrate lazy load and work on bundling/minifying.

--- src/data/work.js
+++ src/data/work.js
@@ -4,89 +4,83 @@ export default [
         title: 'Yamaha E-Bikes',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed leo lectus, blandit non lorem et, convallis scelerisque neque.',
         link: 'https://www.yamahabicycles.com/',
+        image: 'src/images/yamaha-bicycles.PNG'
     },
     {
         id: 2,
         title: 'Yamaha Boats',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed leo lectus, blandit non lorem et, convallis scelerisque neque.',
         link: 'https://www.yamahaboats.com/',
+        image: 'src/images/yamaha-bicycles.PNG'
     },
     {
         id: 3,
         title: 'Yamaha Waverunners',
         description: 'E-commerce',
         link: 'https://yamahawaverunners.com/',
+        image: 'src/images/yamaha-waverunners.PNG'
     },
     {
         id: 4,
         title: 'Yamaha Finance',
         description: 'E-commerce',
         link: 'https://www.yamaha-motor-yes.com/',
+        image: 'src/images/yamaha-finance.PNG'
     },
     {
         id: 5,
         title: 'Woodstock Chimes B2C',
         description: 'E-commerce',
         link: 'https://woodstockchimes.com/',
+        image: 'src/images/woodstock-chimes'
     },
     {
         id: 6,
         title: 'Liberty Furniture Industries Inc',
         description: 'E-commerce',
         link: 'https://www.mylibertyfurniture.com/',
+        image: 'src/images/liberty-furniture'
     },
     {
         id: 7,
         title: 'Lucid Candles',
         description: 'E-commerce',
         link: 'https://www.lucidcandle.com',
+        image: 'src/images/lucid-candles'
     },
     {
         id: 8,
         title: 'Ivystone',
         description: 'E-commerce',
         link: 'https://www.ivystone.com/',
+        image: '~/src/images/ivystone.PNG'
     },
     {
         id: 9,
         title: 'Made Goods',
         description: 'E-commerce',
-        link: 'https://www.madegoods.com/'
+        link: 'https://www.madegoods.com/',
+        image: 'src/images/madegoods.PNG'
     },
     {
         id: 10,
         title: 'Currey and Company',
         description: 'E-commerce, WIP',
-        link: 'http://curreystaging.whereowaretest.com/splash-page/'
+        link: 'http://curreystaging.whereowaretest.com/splash-page/',
+        image: 'src/images/currey.PNG'
     },
     {
         id: 11,
         title: 'Mud Pie',
         description: 'E-commerce',
         link: 'https://www.mud-pie.com/',
+        image: 'src/images/mudpie.PNG'
     },
     {
         id: 12,
         title: 'Mud Pie Wholesale',
         description: 'E-commerce',
         link: 'https://wholesale.mud-pie.com',
+        image: 'src/images/mudpie-wholesale.PNG'
     },
-    {
-        id: 13,
-        title: 'Mud Pie Wholesale',
-        description: 'E-commerce',
-        link: 'https://wholesale.mud-pie.com',
-    },
-    {
-        id: 14,
-        title: 'DEI',
-        description: 'E-commerce',
-        link: 'https://www.deidirect.com'
-    },
-    {
-        id: 15,
-        title: 'Made Goods',
-        description: 'E-commerce',
-        link: 'https://www.madegoods.com/'
-    }
 ];
